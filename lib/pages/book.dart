8d77ad3ad15e1f44b4e9008daee0ab17c761abf4
YemaneAbrha <yemaneabrha21@gmail.com>
2019-10-23 17:10:55 +0300

    Adding font style

--- lib/pages/book.dart
+++ lib/pages/book.dart
@@ -102,160 +102,6 @@ class _BookState extends State<Book> {
       appBar: BasicAppBar.getAppBar(context),
       //backgroundColor: Color.fromRGBO(230, 230, 230, 1.0),
       backgroundColor: Colors.white,
-      //   body: Container(
-      //     padding: EdgeInsets.fromLTRB(10.0, 50.0, 30.0, 32.0),
-      //     child: Center(
-      //       child: ListView(
-      //         children: <Widget>[
-      //           SizedBox(
-      //             height: 10.0,
-      //           ),
-      //           new Row(
-      //             children: <Widget>[
-      //               // new ListView(
-      //               //   children: <Widget>[
-      //               const Icon(
-      //                 Icons.flight,
-      //               ),
-      //               new SizedBox(
-      //                 width: 15,
-      //               ),
-
-      //               new DropdownButton(
-      //                 hint: new Text("FROME *"),
-      //                 value: _departure_city,
-      //                 items: _citiesdropDownMenuItem,
-      //                 onChanged: (String value) {
-      //                   setState(() {
-      //                     _departure_city = value;
-      //                   });
-      //                 },
-      //               ),
-      //               new SizedBox(
-      //                 width: 20.0,
-      //               ),
-
-      //               new DropdownButton(
-      //                 value: _destination_city,
-      //                 hint: new Text("TO *"),
-      //                 items: _citiesdropDownMenuItem,
-      //                 onChanged: (String value) {
-      //                   setState(() {
-      //                     _destination_city = value;
-      //                   });
-      //                 },
-      //               ),
-      //               new Form(
-      //                 key: _formKey,
-      //                 autovalidate: false,
-      //                 child: prefix0.SingleChildScrollView(
-      //                   child: Column(
-      //                     children: <Widget>[
-      //                       Divider(
-      //                           height: 10.0,
-      //                           color: Theme.of(context).primaryColor),
-      //                       DropDownField(
-      //                           value: formData['City'],
-      //                           icon: Icon(Icons.location_city),
-      //                           required: true,
-      //                           hintText: 'Choose a city',
-      //                           labelText: 'City *',
-      //                           items: cities,
-      //                           strict: false,
-      //                           setter: (dynamic newValue) {
-      //                             formData['City'] = newValue;
-      //                           }),
-      //                     ],
-      //                   ),
-      //                 ),
-      //               ),
-      //             ],
-      //           ),
-      //           //   ],
-      //           // ),
-      //           SizedBox(
-      //             height: 16.0,
-      //           ),
-      //           DateTimePickerFormField(
-      //             inputType: InputType.date,
-      //             format: dateFormat,
-      //             editable: false,
-      //             //enabled: false,
-      //             decoration: InputDecoration(
-      //                 icon: Icon(Icons.departure_board),
-      //                 labelText: 'Departure Date *',
-      //                 border: OutlineInputBorder(
-      //                     borderRadius: BorderRadius.circular(10.0)),
-      //                 hasFloatingPlaceholder: false),
-      //           ),
-      //           SizedBox(height: 16.0),
-
-      // new Row(
-      //   children: <Widget>[
-      //     const Icon(Icons.departure_board),
-      //     SizedBox(
-      //       width: 20,
-      //     ),
-      //     new DropdownButton(
-      //       hint: new Text("Departure Time *"),
-      //       value: _departureTime,
-      //       items: _departureTimeDropDownMenuItem,
-      //       onChanged: (String value) {
-      //         setState(() {
-      //           _departureTime = value;
-      //         });
-      //       },
-      //     ),
-      //   ],
-      // ),
-
-      //           SizedBox(
-      //             height: 16.0,
-      //           ),
-      //           _payByOtherPhone
-      //               ? TextField(
-      //                   decoration: new InputDecoration(
-      //                       icon: Icon(Icons.phone),
-      //                       labelText: "Phone Number",
-      //                       border: OutlineInputBorder(
-      //                           borderRadius: BorderRadius.circular(10.0))),
-      //                   keyboardType: TextInputType.text,
-      //                   onChanged: (String value) {
-      //                     setState(() {
-      //                       //_titleValue = value;
-      //                     });
-      //                   },
-      //                 )
-      //               : SwitchListTile(
-      //                   title: const Text("Pay By Other Phone"),
-      //                   value: false,
-      //                   onChanged: (bool value) {
-      //                     setState(() {
-      //                       _payByOtherPhone = true;
-      //                     });
-      //                   },
-      //                   secondary: const Icon(Icons.phone),
-      //                 ),
-      //           SizedBox(
-      //             height: 16.0,
-      //           ),
-      // Center(
-      //   child: RaisedButton(
-      //     child: Text("Check Availiable Buses"),
-      //     color: Color.fromRGBO(0, 136, 204, 0.8),
-      //     onPressed: () {
-      //       setState(() {
-      //         Navigator.pushNamed(context, '/selectBus');
-      //       });
-      //     },
-      //   ),
-      //  )
-      //         ],
-      //       ),
-      //     ),
-      //   ),
-      //   bottomNavigationBar: Buttom(0),
-      // );
       body: Container(
           color: Colors.white,
           constraints: BoxConstraints.expand(),
@@ -276,6 +122,11 @@ class _BookState extends State<Book> {
                           itemsVisibleInDropdown: 1,
                           strict: false,
                           labelText: 'From *',
+                          labelStyle: TextStyle(
+                            fontFamily: 'Raleway',
+                            color: Colors.black,
+                            fontSize: 14.0,
+                          ),
                           items: _cities,
                           setter: (dynamic value) {
                             setState(() {
@@ -290,6 +141,11 @@ class _BookState extends State<Book> {
                           strict: false,
                           itemsVisibleInDropdown: 1,
                           labelText: 'TO',
+                          labelStyle: TextStyle(
+                            fontFamily: 'Raleway',
+                            color: Colors.black,
+                            fontSize: 14.0,
+                          ),
                           items: _cities,
                           setter: (dynamic value) {
                             setState(() {
@@ -308,6 +164,11 @@ class _BookState extends State<Book> {
                   decoration: InputDecoration(
                       icon: Icon(Icons.departure_board),
                       labelText: 'Departure Date *',
+                      labelStyle: TextStyle(
+                        fontFamily: 'Raleway',
+                        color: Colors.black,
+                        fontSize: 20.0,
+                      ),
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(10.0)),
                       hasFloatingPlaceholder: false),
@@ -320,7 +181,14 @@ class _BookState extends State<Book> {
                       width: 20,
                     ),
                     new DropdownButton(
-                      hint: new Text("Departure Time *"),
+                      hint: new Text(
+                        "Departure Time *",
+                        style: TextStyle(
+                          fontFamily: 'Raleway',
+                          color: Colors.black,
+                          fontSize: 20.0,
+                        ),
+                      ),
                       value: _departureTime,
                       items: _departureTimeDropDownMenuItem,
                       onChanged: (String value) {
@@ -337,6 +205,11 @@ class _BookState extends State<Book> {
                         decoration: new InputDecoration(
                             icon: Icon(Icons.phone),
                             labelText: "Phone Number",
+                            labelStyle: TextStyle(
+                              fontFamily: 'Raleway',
+                              color: Colors.black,
+                              fontSize: 20.0,
+                            ),
                             border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(10.0))),
                         keyboardType: TextInputType.text,
@@ -347,7 +220,14 @@ class _BookState extends State<Book> {
                         },
                       )
                     : SwitchListTile(
-                        title: const Text("Pay By Other Phone"),
+                        title: const Text(
+                          "Pay By Other Phone",
+                          style: TextStyle(
+                            fontFamily: 'Raleway',
+                            color: Colors.black,
+                            fontSize: 20.0,
+                          ),
+                        ),
                         value: false,
                         onChanged: (bool value) {
                           setState(() {
