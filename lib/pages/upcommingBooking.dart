b553ee9439bc8f7f083e239c0c97504db338fe7e
YemaneAbrha <yemaneabrha21@gmail.com>
2019-10-24 19:29:42 +0300

    adding card

--- lib/pages/upcommingBooking.dart
+++ lib/pages/upcommingBooking.dart
@@ -5,12 +5,308 @@ class UpcommingBookingPage extends StatefulWidget {
 }
 
 class _UpcommingBookingPageState extends State<UpcommingBookingPage> {
+  List<bool> _isvisiblecontent;
+  @override
+  void initState() {
+    super.initState();
+    _isvisiblecontent = [false, false];
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Container(
-      child: Center(
-        child: Text("Here will be Up comming Booking"),
+      padding: EdgeInsets.fromLTRB(10.0, 20.0, 20.0, 10.0),
+      child: ListView(
+        children: <Widget>[
+          new Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            elevation: 10.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                new Container(
+                  height: 150.0,
+                  child: Image.asset(
+                    'assets/images/Guzo.png',
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+                new Row(
+                  children: <Widget>[
+                    new Expanded(
+                      child: ListTile(
+                        title: Text(
+                          "Selam Bus",
+                          style: TextStyle(fontSize: 20.0),
+                        ),
+                        subtitle: Text("Selam bus form addis ababa to asmera"),
+                      ),
+                      flex: 7,
+                    ),
+                    new Expanded(
+                      child: IconButton(
+                        icon: Icon(Icons.keyboard_arrow_down),
+                        onPressed: () {
+                          setState(() {
+                            _isvisiblecontent[0] = !_isvisiblecontent[0];
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                _isvisiblecontent[0]
+                    ? Container(
+                        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 10.0),
+                        child: Column(
+                          children: <Widget>[
+                            Divider(
+                                height: 20.0,
+                                color: Theme.of(context).primaryColor),
+                            SizedBox(height: 10.0),
+                            Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: Text(
+                                    "From:  ",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Text("Addis Ababa"),
+                                )
+                              ],
+                            ),
+                            SizedBox(height: 10.0),
+                            Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: new Text(
+                                    "To  ",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: new Text("Asmera"),
+                                )
+                              ],
+                            ),
+                            SizedBox(height: 10.0),
+                            Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: new Text(
+                                    "Departure Date: ",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: new Text("tusday,nov 2017"),
+                                )
+                              ],
+                            ),
+                            SizedBox(height: 10.0),
+                            Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: new Text(
+                                    "Depatrture Time:  ",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: new Text("Mornnig"),
+                                )
+                              ],
+                            ),
+                            SizedBox(height: 10.0),
+                            Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: new Text("Fee:  ",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold)),
+                                ),
+                                Expanded(
+                                  child: new Text("1000.00"),
+                                )
+                              ],
+                            ),
+                            SizedBox(height: 10.0),
+                            Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: new Text(
+                                    "Status",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: new Text("payed"),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      )
+                    : SizedBox(
+                        height: 10.0,
+                      )
+              ],
+            ),
+          ),
+          new Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            elevation: 10.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                new Container(
+                  height: 150.0,
+                  child: Image.asset(
+                    'assets/images/Guzo.png',
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+                new Row(
+                  children: <Widget>[
+                    new Expanded(
+                      child: ListTile(
+                        title: Text(
+                          "Selam Bus",
+                          style: TextStyle(fontSize: 20.0),
+                        ),
+                        subtitle: Text("Selam bus form addis ababa to asmera"),
+                      ),
+                      flex: 7,
+                    ),
+                    new Expanded(
+                      child: IconButton(
+                        icon: Icon(Icons.keyboard_arrow_down),
+                        onPressed: () {
+                          setState(() {
+                            _isvisiblecontent[1] = !_isvisiblecontent[1];
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                _isvisiblecontent[1]
+                    ? Container(
+                        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 10.0),
+                        child: Column(
+                          children: <Widget>[
+                            Divider(
+                                height: 20.0,
+                                color: Theme.of(context).primaryColor),
+                            SizedBox(height: 10.0),
+                            Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: Text(
+                                    "From:  ",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Text("Addis Ababa"),
+                                )
+                              ],
+                            ),
+                            SizedBox(height: 10.0),
+                            Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: new Text(
+                                    "To  ",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: new Text("Asmera"),
+                                )
+                              ],
+                            ),
+                            SizedBox(height: 10.0),
+                            Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: new Text(
+                                    "Departure Date: ",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: new Text("tusday,nov 2017"),
+                                )
+                              ],
+                            ),
+                            SizedBox(height: 10.0),
+                            Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: new Text(
+                                    "Depatrture Time:  ",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: new Text("Mornnig"),
+                                )
+                              ],
+                            ),
+                            SizedBox(height: 10.0),
+                            Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: new Text("Fee:  ",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold)),
+                                ),
+                                Expanded(
+                                  child: new Text("1000.00"),
+                                )
+                              ],
+                            ),
+                            SizedBox(height: 10.0),
+                            Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: new Text(
+                                    "Status",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: new Text("payed"),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      )
+                    : SizedBox(
+                        height: 10.0,
+                      )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
